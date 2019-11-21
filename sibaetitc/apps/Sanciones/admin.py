f7e337d9e442d8f012b51fe956c14739180a6b71
davalerova <44624476+davalerova@users.noreply.github.com>
2019-11-21 03:00:35 -0500

    App sanciones

--- sibaetitc/apps/Sanciones/admin.py
+++ sibaetitc/apps/Sanciones/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from apps.afiliacion.models import *
+from .models import *
+
+@admin.register(Tipo_sancion)
+class Tipo_sancionAdmin(admin.ModelAdmin):
+    list_display = ('nombre_sancion', 'duracion_sancion')
+
+@admin.register(Sancion_cliente)
+class Sancion_clienteAdmin(admin.ModelAdmin):
+    list_display = ('afiliacion_servicio_sancion_cliente', 'tipo_sancion', 'descripcion_sancion_cliente', 'created')
--- sibaetitc/apps/Sanciones/models.py
+++ sibaetitc/apps/Sanciones/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from apps.afiliacion.models import *
 
 # Create your models here.
+class TimeStampModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Tipo_sancion(TimeStampModel):
+    nombre_sancion = models.CharField(help_text='Nombre de la sanción', verbose_name='Nombre sanción', max_length=50)
+    duracion_sancion = models.IntegerField(help_text='Cantidad de días que dura la sanción', verbose_name='Duración sanción')
+
+    def __str__(self):
+        return self.nombre_sancion + self.duracion_sancion
+    class Meta:
+        verbose_name = 'Tipo de sanción'
+        verbose_name_plural = 'Tipos de sanción'
+
+class Sancion_cliente(TimeStampModel):
+    afiliacion_servicio_sancion_cliente = models.ForeignKey(Afiliacion_servicio, help_text='Afiliación servicio a la que se aplica la sanción', verbose_name='Sanción cliente', on_delete=False)
+    tipo_sancion = models.ForeignKey(Tipo_sancion, help_text='Tipo de sanción que se va a aplicar', verbose_name='Tipo de sanción', on_delete=False)
+    descripcion_sancion_cliente = models.TextField(help_text='Descripción de la sanción', verbose_name='Descripción de la sanción')
+
+    def __str__(self):
+        self.afiliacion_servicio_sancion_cliente + self.tipo_sancion + self.created
+
+
+
+    class Meta:
+        verbose_name = 'Sanción cliente'
+        verbose_name_plural = 'Sanciones cliente'
+
